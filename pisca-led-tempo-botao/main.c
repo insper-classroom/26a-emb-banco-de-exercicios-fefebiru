17ceb0d7ef3f610cbe0accd7bd5e8ab0707b47f4
fefebiru <felipet4@al.insper.edu.br>
2026-03-25 00:51:32 -0300

    Pisca-led-tempo-botao v5

--- pisca-led-tempo-botao/main.c
+++ pisca-led-tempo-botao/main.c
@@ -75,7 +75,7 @@ int main() {
             tempo_f = get_absolute_time();
             int64_t duration = absolute_time_diff_us(tempo_i,tempo_f);
             led_state = 0;
-            add_repeating_timer_us(duration / 2, timer_callback, NULL, &timer_id);
+            add_repeating_timer_us(duration, timer_callback, NULL, &timer_id);
         }
     }
     return 0;
